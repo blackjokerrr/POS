f30fd8727849fb9d13ae4b9fda8733f128982a7f
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2020-03-12 00:36:51 +0700

    Finish

--- Report/views.py
+++ Report/views.py
@@ -2,9 +2,37 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
+from Sale.models import Order_Product, Order_Product_Storage
+from Management.models import Product, Order
 
 # Create your views here.
 
 def report(request):
     
-    return HttpResponse('1')
+    name_all = Product.objects.all()
+    product_out = Order_Product_Storage.objects.all()
+    listed_value, check_same_name = [], ''
+    check_listed, total = [], 0
+    dict_of_sum = {}
+    temp = []
+    
+    for value_of_sum in product_out:
+        if value_of_sum.storage_product.name not in check_listed:
+            check_listed.append(value_of_sum.storage_product.name)
+            dict_of_sum[value_of_sum.storage_product.name] = value_of_sum.storage_amount_all
+        else:
+            dict_of_sum[value_of_sum.storage_product.name] += value_of_sum.storage_amount_all
+    
+    
+    for dict_items in check_listed:
+        temp.append(dict_of_sum[dict_items])
+
+    
+
+    
+    return render(request, 'Report/report.html', context={
+        'name': request.user.username,
+        'product_sum_all': dict_of_sum,
+        'product_name': check_listed,
+        'total_item': temp
+    })
---
+++ static/js/script.js
@@ -0,0 +1,189 @@
+
+Chart.defaults.global.defaultFontFamily = 'Itim';
+Chart.defaults.global.defaultFontSize = 18;
+
+let charts = document.getElementById('Charts').getContext('2d');
+let charts_week = document.getElementById('Charts_week').getContext('2d');
+let charts_month = document.getElementById('Charts_month').getContext('2d');
+let charts_year = document.getElementById('Charts_year').getContext('2d');
+
+let product_name = [];
+let product_total = [];
+let date = new Date();
+let loop = document.getElementById('total_product').innerHTML;
+
+let color = [];
+
+for (let travel = 1; travel <= loop; travel++) {
+    product_name.push(document.getElementById('' + travel).innerHTML);
+    product_total.push(document.getElementById('t' + travel).innerHTML);
+
+}
+
+for (let i=0;i<loop;i++){
+    color.push('rgba(255,' + Math.floor(Math.random() * 100) + ',' + Math.floor(Math.random() * 100) + ',' + '0.5)');
+}
+console.log(color);
+
+let show_day = new Chart(charts, {
+    type: 'bar',
+    data: {
+        labels: product_name,
+        datasets: [{
+            barPercentage: 1.0,
+            label: 'สินค้าที่นิยมซื้อมากที่สุด',
+            data: product_total,
+            backgroundColor: color,
+            borderWidth: 4,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderWidth: '#000',
+
+        }],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Chart of Day',
+            fontSize: 32
+        },
+        legend: {
+            position: 'right',
+            labels: {
+                fontColor: '#000',
+                fontSize: 20
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        }
+    }
+});
+
+
+let show_month = new Chart(charts_week, {
+    type: 'bar',
+    data: {
+        labels: product_name,
+        datasets: [{
+            barPercentage: 1.0,
+            label: 'สินค้าที่นิยมซื้อมากที่สุด',
+            data: product_total,
+            backgroundColor: color,
+            borderWidth: 4,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderWidth: '#000',
+
+        }],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Chart of Week',
+            fontSize: 32
+        },
+        legend: {
+            position: 'right',
+            labels: {
+                fontColor: '#000',
+                fontSize: 20
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        }
+    }
+});
+
+
+let show_year = new Chart(charts_month, {
+    type: 'bar',
+    data: {
+        labels: product_name,
+        datasets: [{
+            barPercentage: 1.0,
+            label: 'สินค้าที่นิยมซื้อมากที่สุด',
+            data: product_total,
+            backgroundColor: color,
+            borderWidth: 4,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderWidth: '#000',
+
+        }],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Chart of Month',
+            fontSize: 32
+        },
+        legend: {
+            position: 'right',
+            labels: {
+                fontColor: '#000',
+                fontSize: 20
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        }
+    }
+});
+
+
+let Show_pop = new Chart(charts_year, {
+    type: 'bar',
+    data: {
+        labels: product_name,
+        datasets: [{
+            barPercentage: 1.0,
+            label: 'สินค้าที่นิยมซื้อมากที่สุด',
+            data: product_total,
+            backgroundColor: color,
+            borderWidth: 4,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderWidth: '#000',
+
+        }],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Chart of Year',
+            fontSize: 32
+        },
+        legend: {
+            position: 'right',
+            labels: {
+                fontColor: '#000',
+                fontSize: 20
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                }
+            }]
+        }
+    }
+});
